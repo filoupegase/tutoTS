4a6e36c287e3709ad71a6d99abeef957f5a0c34f
filoupegase <loisoncorentin@yahoo.fr>
2022-04-01 14:33:28 +0200

    fix ts

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,10 @@
-console.log('hello');
+const counter = document.querySelector('#counter');
+let i = 0;
+
+const increment = (e) => {
+    i++;
+    counter.querySelector('span').innerText = i.toString()
+}
+
+counter.addEventListener('click', increment)
+
