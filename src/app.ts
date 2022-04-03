9ac5a13c264618031e56778e697f9ac4308a3112
filoupegase <loisoncorentin@yahoo.fr>
2022-04-03 19:03:30 +0200

    feat/save ts

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,20 @@
-const counter = document.querySelector('#counter');
-let i = 0;
+const user: { firstName: string, lastName?: string, age: number } = {firstName: 'John', age: 20};
+const date: Date = new Date();
+const cb: (e: MouseEvent) => void = (e: MouseEvent): number => {
+    return 3
+};
 
-const increment = (e) => {
+function printId(id: number): void {
+    console.log(id.toString());
+}
+
+const counter = document.querySelector('#counter');
+let i: number = 0;
+
+const increment = (e: MouseEvent): void => {
     i++;
     counter.querySelector('span').innerText = i.toString()
 }
 
-counter.addEventListener('click', increment)
+counter.addEventListener('click', increment);
 
